6db6e95126ec0c32a9fd293f4ee6fba8d8944b93
Dan Gopstein <dan@gopstein.com>
2016-10-18 16:16:53 -0400

    Print include file names

--- src/atom_finder/core.clj
+++ src/atom_finder/core.clj
@@ -1,12 +1,30 @@
-(ns atom-finder.core)
-
-(import org.eclipse.cdt.core.parser.FileContent)
+(ns atom-finder.core
+  (:import [org.eclipse.cdt.core.parser DefaultLogService FileContent IncludeFileContentProvider ScannerInfo]
+           [org.eclipse.cdt.core.dom.ast gnu.cpp.GPPLanguage ASTVisitor]))
 
 (defn file-content
   "read in source file to be analyzed"
   [filename]
   (FileContent/createForExternalFileLocation filename))
 
+(defn file-includes
+  "list all includes in file"
+  [filename]
+  (let [definedSymbols {}
+        includePaths (make-array String 0)
+        info (new ScannerInfo definedSymbols includePaths)
+        log (new DefaultLogService)
+        emptyIncludes (IncludeFileContentProvider/getEmptyFilesProvider)
+        opts 8
+        translationUnit (.getASTTranslationUnit (GPPLanguage/getDefault)
+                                                (file-content filename) info emptyIncludes nil opts log)]
+
+    (.getIncludeDirectives translationUnit)))
+
+(def filename "/home/dgopstein/nyu/confusion/atoms/simplifications/2000-natori/nonconfusing.c")
+
 (defn -main
   [& args]
-  (prn (file-content "/home/dgopstein/nyu/confusion/atoms/simplifications/1984-anonymous/nonconfusing.c")))
+  (prn (file-content filename))
+  (for [incld (file-includes filename)] (printf "include - %s\n" (.getName incld)))
+  )
