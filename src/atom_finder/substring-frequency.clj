e72f3167c6af159ac029f861c3d791c00ff7abdd
Dan Gopstein <dan@gopstein.com>
2016-10-21 10:11:52 -0400

    Count all substrings the easy way

--- src/atom_finder/substring-frequency.clj
+++ src/atom_finder/substring-frequency.clj
@@ -1,6 +1,6 @@
-(eq (substring-freq "aaa")  ([\a 3 ([\a 2 ([\a 1])])]))
-(eq (substring-freq "aab")  ([\a 2 ([\a 1] [\b 1])] [\b 1]))
-(eq (substring-freq "abc")  ([\a 1]))
+(= (subs-freq "aaa" 3)  ([\a 3 ([\a 2 ([\a 1])])]))
+(= (subs-freq "aab" 3)  ([\a 2 ([\a 1] [\b 1])] [\b 1]))
+(= (subs-freq "abc" 3)  ([\a 1]))
 
 (defrecord FreqNode [element count children])
 
@@ -8,28 +8,48 @@
 (def test-node (->FreqNode \a 2 {\a (->FreqNode \a 1 {})}))
 (-> test-node
     :children
-    \a
+    (get \a)
     :children)
 
 
 
 (defn inc-add
-  "If a child exists, increment it, other wise add it"
+  "If a child exists, increment it, otherwise add it"
   [node element]
-  (update-in node [:children element] #(if (nil? (:count %1)) (->FreqNode element 1 {}) (update %1 :count inc))))
+  (update-in node [:children element]
+             #(if (nil? (:count %1))
+                (->FreqNode element 1 {})
+                (update %1 :count inc))))
 
 (inc-add (inc-add test-node \a) \a)
 
-(defn substring-freq
-  "Count the occurences of every substring below length m in string s"
-  ([str m]
-   (substring-freq (rest str) m
-                   (let [root (->FreqNode (first str) 1 {})]
-                     (for [lifetime (range 50)] [lifetime root]))))
+;; (defn substring-freq
+;;   "Count the occurences of every substring below length m in string s"
+;;   ([str m]
+;;    (substring-freq (rest str) m
+;;                    (let [root (->FreqNode (first str) 1 {})]
+;;                      (for [lifetime (range 50)] [lifetime root]))))
    
-  ([str m nodes]
-   (for [char str
-         node nodes]
-     (:children node)
+;;   ([str m nodes]
+;;    (let [char (first str)]
+;;      (for [node nodes]
+;;        (inc-add node (:children node))
+
+;;        )
+       
     
-     )))
+;;      ))))
+
+
+(defn all-subs [str m]
+  (let [str-len (count str)]
+    (for [start (range 0 str-len)
+          end   (range (+ start 1) (min (+ start m 1) str-len))]
+      (do
+        (prn [start end])
+        (subs str start end)))))
+
+(defn subs-freq [str max-len]
+  (frequencies (all-subs str max-len)))
+
+(subs-freq "aabc" 8)
