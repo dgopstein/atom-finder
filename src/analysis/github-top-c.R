ddd993506942a3650e1996ba02d8862d28d19c17
Dan Gopstein <dan@gopstein.com>
2016-11-15 11:02:04 -0500

    Find ast node counts in most popular github c projects

---
+++ src/analysis/github-top-c.R
@@ -0,0 +1,14 @@
+library(data.table)
+
+# project, node, count
+github.top.c <- data.table(read.csv('../data/github-top-c.csv'))
+
+project.size <- github.top.c[, .(count = sum(count)), by=project][order(-count)]
+
+github.top.c <- merge(github.top.c, project.size, by='project', suffixes = c('.type', '.total'))
+
+github.top.c[, rate:=(count.type/count.total)]
+
+node.rates <- github.top.c[, .(rate = mean(rate)), by=node][order(-rate)]
+
+write.csv(node.rates)
