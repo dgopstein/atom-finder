49ab6e9b2838f32ff13a607d9302b4d5cbb4dd51
Dan Gopstein <dan@gopstein.com>
2016-11-28 20:31:44 -0500

    Tests compile

--- test/atom_finder/source_versions_test.clj
+++ test/atom_finder/source_versions_test.clj
@@ -6,16 +6,16 @@
             ))
 
 
-(deftest context-lines
+(deftest context-lines-test
   (testing "Parsing the @@ lines of a patch file"
     (is (= (context-lines "@@ -1,2 +3,4 @@") [[1 2 3 4]])
     (is (= (context-lines "@@ -1 +3 @@") [[1 1 3 1]]))
-    ))
+    )))
 
-  (deftest removed-lines
-    (testing "Which lines were removed in this patch"
-      (let [patch (->> "97574c57cf26ace9b8609575bbab66465924fef7_partial.patch" resource-path slurp)
-            rl    (removed-lines patch)]
-            
-        (is (= rl {"gcc/ChangeLog" [[16 1] [26 4]] "gcc/config/sparc/sparc.c" []}))
-  )))
+(deftest removed-lines-test
+  (testing "Which lines were removed in this patch"
+    (let [patch (->> "97574c57cf26ace9b8609575bbab66465924fef7_partial.patch" resource-path slurp)
+          rl    (removed-lines patch)]
+      
+      (is (= rl {"gcc/ChangeLog" [[16 1] [26 4]] "gcc/config/sparc/sparc.c" []}))
+      )))
