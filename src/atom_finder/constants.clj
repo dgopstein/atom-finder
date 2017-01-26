7f7601210d50cb2edb4b36263d02fd2911f61976
jakeinc <jakeiannacone@optonline.net>
2017-01-26 18:32:36 -0500

    Implement JSON conf file
    Uses repo locations are specified in json.conf

--- src/atom_finder/constants.clj
+++ src/atom_finder/constants.clj
@@ -1,13 +1,16 @@
 (ns atom-finder.constants
   (:require [atom-finder.util :refer :all]
-            [clj-jgit.porcelain :as gitp]))
+            [clj-jgit.porcelain :as gitp]
+            [clojure.data.json :as json]))
 
-(def gcc-path (expand-home "~/opt/src/gcc"))
-(def gcc-repo (gitp/load-repo gcc-path))
+(def conf-data (json/read-str (slurp (resource-path "conf.json")):key-fn keyword))
 
-(def ag-path (expand-home "~/opt/src/the_silver_searcher"))
+(def gcc-path (expand-home (get conf-data :gcc-path)))
+(def gcc-repo (gitp/load-repo "Z:\\Github\\gcc"))
+
+(def ag-path (expand-home (get conf-data :ag-path)))
 (def ag-repo (gitp/load-repo ag-path))
 
 (def root (tu (resource-path "logic-as-control-flow.c")))
-(def big-root (tu (expand-home "~/opt/src/github-top-c/php-src/ext/sqlite3/libsqlite/sqlite3.c")))
-(def github-top-c (expand-home "~/opt/src/github-top-c"))
+(def big-root (tu (expand-home (get conf-data :big-root))))
+(def github-top-c (expand-home (get conf-data :github-top-c)))
