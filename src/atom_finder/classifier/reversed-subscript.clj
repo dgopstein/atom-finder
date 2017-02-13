5f6b09d6712411a07f85feb3df4c9f99b0f7b14d
Normand-1024 <hawtherabbit@gmail.com>
2017-02-13 09:22:23 -0500

    Added comments in the classifier

--- src/atom_finder/classifier/reversed-subscript.clj
+++ src/atom_finder/classifier/reversed-subscript.clj
@@ -1,8 +1,11 @@
 (in-ns 'atom-finder.classifier)
-(import '(org.eclipse.cdt.core.dom.ast IASTArraySubscriptExpression))
+(import '(org.eclipse.cdt.core.dom.ast IASTArraySubscriptExpression IBasicType))
 
 (defn reversed-subscript-atom?
   "Is this node a reversed-subscript-atom?"
   [node] 
   (and (instance? IASTArraySubscriptExpression node)
-       (instance? org.eclipse.cdt.core.dom.ast.IBasicType(.getExpressionType (first (children node))))))
+       ;;the first child will be the element to the left of the squared bracket
+       ;;.getExpressionType returns the type that the expression evaluates to
+       ;;IBasicType covers types that can be used as indices
+       (instance? IBasicType (.getExpressionType (first (children node))))))
