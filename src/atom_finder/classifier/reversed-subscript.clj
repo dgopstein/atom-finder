484b1a6e8f6f09ddefd48c0af0875b4e8bce35d5
Dan Gopstein <dan@gopstein.com>
2017-02-13 01:11:12 -0500

    Make tests compile and add a few extras

--- src/atom_finder/classifier/reversed-subscript.clj
+++ src/atom_finder/classifier/reversed-subscript.clj
@@ -6,18 +6,3 @@
   [node] 
   (and (instance? IASTArraySubscriptExpression node)
        (instance? org.eclipse.cdt.core.dom.ast.IBasicType(.getExpressionType (first (children node))))))
-
-(comment
-
-(->>
-  "reversed-subscript.c"
-  resource-path
-  tu
-  (get-in-tree [5 2])
-  children
-  (filter #(#{"ExpressionStatement"} (typename %)))
-  (map (partial get-in-tree [0 2]))
-  (map (fn [node]
-            [(= "int" (.toString (.getExpressionType (first (children node)))))
-             (write-ast node)]))
- )
