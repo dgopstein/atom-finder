5103def82f6e1bfd657dd21a469a45e81e47e508
Dan Gopstein <dan@gopstein.com>
2017-02-28 23:40:06 -0500

    travis-ci test setup

--- src/atom_finder/constants.clj
+++ src/atom_finder/constants.clj
@@ -3,13 +3,16 @@
             [clj-jgit.porcelain :as gitp]
             [omniconf.core :as cfg]))
 
- (cfg/populate-from-file (resource-path "conf.edn"))
-(def gcc-path (expand-home  (cfg/get :gcc-path)))
-(def gcc-repo (gitp/load-repo gcc-path))
+(cfg/populate-from-file (resource-path "conf.edn"))
 
-(def ag-path (expand-home  (cfg/get :ag-path)))
-(def ag-repo (gitp/load-repo ag-path))
+
+(def gcc-path (some->> :gcc-path cfg/get expand-home))
+(def ag-path  (some->> :ag-path  cfg/get expand-home))
+
+(def gcc-repo (some->> gcc-path gitp/load-repo))
+(def ag-repo  (some->> ag-path  gitp/load-repo))
 
 (def root (tu (resource-path "logic-as-control-flow.c")))
-(def big-root (tu (expand-home  (cfg/get :big-root))))
-(def github-top-c (expand-home  (cfg/get :github-top-c)))
+
+(def big-root     (some->> :big-root     cfg/get expand-home tu))
+(def github-top-c (some->> :github-top-c cfg/get expand-home))
