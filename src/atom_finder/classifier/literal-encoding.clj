8a7424fb9b0329407e1ac0ee6e34c25196470168
Dan Gopstein <dan@gopstein.com>
2017-03-01 22:07:39 -0500

    Literal Encoding radix tests

--- src/atom_finder/classifier/literal-encoding.clj
+++ src/atom_finder/classifier/literal-encoding.clj
@@ -1,15 +1,32 @@
 (in-ns 'atom-finder.classifier)
+(import '(org.eclipse.cdt.core.dom.ast IASTNode IASTBinaryExpression IASTUnaryExpression))
 
 (defmulti radix "In which base is the number specified" class)
 (s/defmethod radix String :- s/Keyword
   [num :- s/Str]
   ; This is a simplification. See for details:
   ; cdt/core/org.eclipse.cdt.core/parser/org/eclipse/cdt/internal/core/dom/parser/cpp/CPPASTLiteralExpression.java
-  (condp (flip clojure.string/starts-with?) num
-    "0x" :hex
-    "0b" :bin
-    "0"  :oct
-         :dec
+  (condp re-find num
+    #"^[+-]?0x" :hex
+    #"^[+-]?0b" :bin
+    #"^[+-]?0([^0-9]|$)" :dec ; Matches "0", "0.1" This may not technically be true buuuut...
+    #"^[+-]?0.[0-9]*[.eE]" :dec ; There is no octal float in C
+    #"^[+-]?0"  :oct
+                :dec
   ))
-(s/defmethod radix IASTNode [n :- IASTNode]
+
+(s/defmethod radix IASTNode :- s/Keyword [n :- IASTNode]
   (radix (String. (.getValue n))))
+
+(s/defmethod bitwise-op? s/Bool [n :- IASTNode]
+  (condp instance
+      IASTBinaryExpression (bitwise-binary-op? n)
+      IASTUnaryExpression  (bitwise-unary-op? n)))
+
+&  bitwise AND
+|  bitwise inclusive OR
+^  bitwise XOR (eXclusive OR)
+<<  left shift
+>>  right shift
+~  bitwise NOT (one's complement) (unary)
+
---
+++ test/atom_finder/literal_encoding_test.clj
@@ -0,0 +1,40 @@
+(ns atom-finder.classifier-test
+  (:require [clojure.test :refer :all]
+            [schema.test]
+            [atom-finder.util :refer :all]
+            [atom-finder.classifier :refer :all]
+            ))
+
+(use-fixtures :once schema.test/validate-schemas)
+
+(deftest test-radix
+  (testing "Determine the radix of string literals"
+    (let [cases [
+          ["123"   :dec]
+          ["012"   :oct]
+          ["0x2"   :hex]
+          ["0b1"   :bin]
+          ["0b0"   :bin]
+          ["0"     :dec]
+          ["0.0"   :dec]
+          ["0.1"   :dec]
+          [".0"    :dec]
+          [".1"    :dec]
+          ["0x0"   :hex]
+          ["00"    :oct]
+          ["0123.456" :dec]
+          ["0x123.456p0" :hex]
+          ["0x123.456p1" :hex]
+          ["0x123.456p2" :hex]
+          ["012e2" :dec]
+                 ]
+          signed-cases (mapcat (fn [[s b]] [[s b]
+                                          [(str "-" s) b]
+                                          [(str "+" s) b]]) cases)
+          ]
+
+    (doseq [[literal base] signed-cases]
+      (testing (str "Which base is " literal " - " base)
+        (is (= base (radix literal)))))
+
+      )))
