b5f1af13c84f3b71a3e235a24aca88d155577f17
Dan Gopstein <dan@gopstein.com>
2017-03-05 19:05:07 -0500

    Remove assignment-as-value.clj

--- src/atom_finder/classifier/assignment-as-value.clj
+++
@@ -1,4 +0,0 @@
-(in-ns 'atom-finder.classifier)
-(import '(org.eclipse.cdt.core.dom.ast IASTNode IASTBinaryExpression IASTUnaryExpression IASTExpressionStatement))
-
-(s/defn assignment-as-value-atoms [n] [])
