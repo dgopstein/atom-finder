2b122dc3b765502e994321a740861a5fc012f5d6
Dan Gopstein <dan@gopstein.com>
2017-03-20 17:45:32 -0400

    Add failing test

---
+++ test/atom_finder/omitted_curly_braces_test.clj
@@ -0,0 +1,29 @@
+(ns atom-finder.omitted-curly-braces-test
+  (:require [clojure.test :refer :all]
+            [schema.test]
+            [atom-finder.util :refer :all]
+            [atom-finder.classifier-util :refer :all]
+            [atom-finder.classifier :refer :all]
+            [atom-finder.constants :refer :all]
+            ))
+
+(use-fixtures :once schema.test/validate-schemas)
+
+(deftest test-omitted-curly-braces-atom?
+  (->> "curly-braces-in-macro.c"
+       resource-path
+       slurp
+       parse-source
+       ((default-finder omitted-curly-braces-atom?)))
+
+  ;(->> "curly-braces-in-macro.c"
+  ;     resource-path
+  ;     slurp
+  ;     parse-source
+  ;     (get-in-tree [0 2 0 0 0])
+  ;     (get-in-tree [1])
+  ;     .getSyntax
+  ;     )
+
+  (testing "omitted-curly-braces-atom? finds all atoms in snippet study code"
+    (test-atom-lines "omitted-curly-braces.c" "<true>" (default-finder omitted-curly-braces-atom?))))
