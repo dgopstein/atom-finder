8ac7ed97c15da0b7ed40629123629cc0c564bc84
Dan Gopstein <dan@gopstein.com>
2017-04-12 10:39:05 -0400

    Add tests

--- src/test/resources/infix-operator-precedence.c
+++ src/test/resources/infix-operator-precedence.c
@@ -11,4 +11,14 @@ int main(){
   int a = 3 + 4 * 5 / 8 - 4; // <true>
   int b = 3 + 4 + 5;
   a = 3 + 4;
+
+  a = b || a = c; // <true>
+
+  a = 1, 2; // <true>
+
+  b = a = 1; // <false>
+
+  1 + 2 - 3; // <false>
+
+  a ? 1 ? b : 2 : 3; // <true> - Although I could be persuaded to ignore this example
 }
