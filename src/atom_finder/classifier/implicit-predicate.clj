e1f5e456064c1b38de0a1420b75033c7e4779496
jakeinc <jakeiannacone@optonline.net>
2017-05-01 14:50:13 -0400

    Update implicit-predicate.clj

--- src/atom_finder/classifier/implicit-predicate.clj
+++ src/atom_finder/classifier/implicit-predicate.clj
@@ -1,38 +1,38 @@
 (in-ns 'atom-finder.classifier)
-(import '(org.eclipse.cdt.core.dom.ast  IASTIfStatement IASTForStatement IASTWhileStatement IASTDoStatement IASTBinaryExpression))
+(import '(org.eclipse.cdt.core.dom.ast  IASTIfStatement IASTForStatement IASTWhileStatement IASTDoStatement IASTBinaryExpression IASTUnaryExpression))
 
-(def logical-operators #{IASTBinaryExpression/op_equals
-                        IASTBinaryExpression/op_greaterEqual
-                        IASTBinaryExpression/op_greaterThan
-                        IASTBinaryExpression/op_lessThan
-                        IASTBinaryExpression/op_lessEqual
-                        IASTBinaryExpression/op_logicalAnd
-                        IASTBinaryExpression/op_logicalOr
-                        IASTBinaryExpression/op_notequals})
+(def implicit-types #{IASTIfStatement IASTForStatement IASTWhileStatement IASTDoStatement IASTConditionalExpression})
+; If statement as an implicit type:  C99 6.8.4.1/1
+; For/While/Do statements as implicit types:  C99 6.8.5/2
+; Ternary operator as an implicit type:  C99 6.5.15/1
 
-(defn implicit-preciate-for-if-statement?
-  "Is there an implicit predicate in this if or for statement"
-  [node]
-  (cond
-    (instance? IASTBinaryExpression (.getConditionExpression node))
-      (not (contains? logical-operators (.getOperator (.getConditionExpression node))))
-    :else true))
+(declare implicit-expression?)
 
-(defn implicit-predicate-while-do-loop?
-  "Is there an implicit predicate in this do-while loop"
-  [node]
-  (cond
-    (instance? IASTBinaryExpression (.getCondition node))
-      (not (contains? logical-operators (.getOperator (.getCondition node))))
-    :else true))
+(defn remove-parentheses
+[expr]
+(cond
+  (== IASTUnaryExpression/op_not (.getOperator expr)) true
+  (== IASTUnaryExpression/op_bracketedPrimary (.getOperator expr))
+    (not (implicit-expression? (.getOperand expr)))
+  :else false))
+
+(defn implicit-expression?
+  "Does this expression have a logical operator in it?"
+  [expr]  
+  (not (cond
+    (instance? IASTBinaryExpression expr)
+      (contains? logical-operators (.getOperator expr))
+    (instance? IASTUnaryExpression  expr)
+      (remove-parentheses expr)
+    :else false)))
 
 (defn implicit-predicate-atom?
   "Does this AST node have an implicit predicate atom"
   [node]
   (cond
-   (leaf? node) nil
-   (instance? IASTForStatement node) (implicit-preciate-for-if-statement? node)
-   (instance? IASTIfStatement node) (implicit-preciate-for-if-statement? node)
-   (instance? IASTWhileStatement node) (implicit-predicate-while-do-loop? node)
-   (instance? IASTDoStatement node) (implicit-predicate-while-do-loop? node)
-   :else false))
+    (instance? IASTForStatement node) (implicit-expression? (.getConditionExpression node))
+    (instance? IASTIfStatement node) (implicit-expression? (.getConditionExpression node))
+    (instance? IASTWhileStatement node) (implicit-expression? (.getCondition node))
+    (instance? IASTDoStatement node) (implicit-expression? (.getCondition node))
+    (instance? IASTConditionalExpression node) (implicit-expression? (.getLogicalConditionExpression node))
+    :else false))
