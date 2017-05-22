6067a45b703360a37a02b3cb7bcfb2d869720e97
Dan Gopstein <dan@gopstein.com>
2017-05-22 12:39:58 -0400

    write-node

---
+++ src/atom_finder/write-node.clj
@@ -0,0 +1,46 @@
+(ns atom-finder.core
+  (:require [atom-finder.util :refer :all]
+            [schema.core :as s]
+            [clojure.pprint :refer [pprint]]
+            [clojure.java.io :as io]
+            )
+  (:import )
+  )
+
+(import '(org.eclipse.cdt.internal.core.dom.rewrite.astwriter ASTWriter ASTWriterVisitor))
+(import '(org.eclipse.cdt.internal.core.dom.rewrite ASTModificationStore))
+(import '(org.eclipse.cdt.internal.core.dom.rewrite.commenthandler NodeCommentMap))
+(import '(org.eclipse.cdt.internal.core.dom.rewrite.changegenerator ChangeGeneratorWriterVisitor))
+
+(defmacro visit-nothing [writer]
+  "Tell the AST-visitor to visit no types of node"
+  (let [fields
+        ['shouldVisitArrayModifiers 'shouldVisitBaseSpecifiers
+         'shouldVisitDeclarations 'shouldVisitDeclarators 'shouldVisitDeclSpecifiers
+         'shouldVisitDecltypeSpecifiers 'shouldVisitExpressions
+         'shouldVisitInitializers 'shouldVisitNames 'shouldVisitNamespaces
+         'shouldVisitParameterDeclarations 'shouldVisitPointerOperators
+         'shouldVisitStatements 'shouldVisitTemplateParameters
+         'shouldVisitTranslationUnit 'shouldVisitTypeIds 'shouldVisitAttributes]]
+    (concat '(do)
+            (for [field fields]
+              `(set! (. ~writer ~field) false)))))
+
+
+(defn write-node [node]
+  (let [modificationStore (ASTModificationStore.)
+        commentMap (NodeCommentMap.)
+        writer (ChangeGeneratorWriterVisitor. modificationStore nil commentMap)]
+
+    (visit-nothing writer)
+
+		(when (not (nil? node))
+			(.accept node writer))
+
+		(.toString writer)
+  ))
+
+(->> "a + b"
+     parse-frag
+     write-node)
+     ;write-ast)
