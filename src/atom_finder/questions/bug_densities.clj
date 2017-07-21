1f99c5b47a9eb1417df91db5fa98cf8dbd5cf43a
Dan Gopstein <dan@gopstein.com>
2017-07-20 22:01:14 -0400

    Add non-atom counts

--- src/atom_finder/questions/bug_densities.clj
+++ src/atom_finder/questions/bug_densities.clj
@@ -31,14 +31,23 @@
 (defn atom-and-bug-counts
   "For a given rev-commit, count how many bugs (and atoms before) are in the file"
   [srcss]
-  (let [n-bugs (->> srcss :rev-commit bugzilla-ids count)
-        n-atoms (->> srcss :srcs (map :atoms-before) (mapcat vals) flatten count)]
-    {:rev-str (:rev-str srcss) :n-bugs n-bugs :n-atoms n-atoms}))
+  {:rev-str     (->> srcss :rev-str)
+   :n-bugs      (->> srcss :rev-commit bugzilla-ids count)
+   :n-atoms     (->> srcss :srcs (map :atoms-before) (mapcat vals) flatten count)
+   :n-non-atoms (->> srcss :srcs (map :non-atoms-before) flatten count)
+   })
 
 '(->> gcc-repo
     (map-all-commits atom-and-bug-counts)
-    (take 2)
+    (take 100)
     (map prn)
     dorun
-     ;(log-to "tmp/bug-densities-2017-07-20.txt")
+    (log-to "tmp/bug-densities-2017-07-20.txt")
     time-mins)
+
+'(->> "tmp/bug-densities-2017-07-20.txt"
+     read-lines
+     (map #(dissoc % :rev-str))
+     (group-by (comp pos? :n-bugs))
+     (map-values (partial reduce (partial merge-with +)))
+     pprint)
