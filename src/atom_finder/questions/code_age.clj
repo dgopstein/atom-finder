50c7aa9252369aa5a78a1c110efbfcfa5999f415
Dan Gopstein <dan@gopstein.com>
2017-10-13 10:16:48 -0400

    list all file content in repo revision

--- src/atom_finder/questions/code_age.clj
+++ src/atom_finder/questions/code_age.clj
@@ -18,8 +18,10 @@
    [schema.core :as s]
    )
   (:import
-   [org.eclipse.jgit.api Git]
-   [org.eclipse.jgit.treewalk CanonicalTreeParser]))
+   [org.eclipse.jgit
+    api.Git lib.Repository lib.ObjectId
+    revwalk.RevCommit revwalk.RevWalk
+    treewalk.CanonicalTreeParser treewalk.TreeWalk]))
 
 ;; https://stackoverflow.com/questions/7744656/how-do-i-filter-elements-from-a-sequence-based-on-indexes
 (defn filter-by-index [coll idxs]
@@ -49,27 +51,41 @@
        (partition-by #(-<> % .getCommitTime long sec->java-time year-month))
        (map last)))
 
+;; https://stackoverflow.com/questions/1685228/how-to-cat-a-file-in-jgit
+(s/defn treewalk->filestr
+  [repo :- Repository tree-walk :- TreeWalk]
+  (-<>> tree-walk (.getObjectId <> 0) (.open repo) .getBytes String.))
+
+(s/defn treewalk->file :- {:path s/Str :content s/Str}
+  [repo :- Repository tree-walk :- TreeWalk]
+  {:path    (->> tree-walk .getPathString)
+   :content (->> tree-walk (treewalk->filestr repo))})
+
 ;; https://stackoverflow.com/questions/19941597/use-jgit-treewalk-to-list-files-and-folders
-(s/defn list-repository-contents [git-repo :- Git commit]
+(def RevFile {:path s/Str :rev-str s/Str :content s/Str})
+(s/defn repo-files :- [RevFile]
+  "Return the content of every file in repository at given commit"
+  [git-repo :- Git commit :- RevCommit]
   (let [repository (.getRepository git-repo)
-        ;head (.getRef repository "HEAD") ;TODO NOT HEAD
-        walk (org.eclipse.jgit.revwalk.RevWalk. repository) ; probably can be the commit
-        ;commit (.parseCommit walk (.getObjectId head))
+        walk (org.eclipse.jgit.revwalk.RevWalk. repository)
         tree   (.getTree commit)
-        treeWalk (org.eclipse.jgit.treewalk.TreeWalk. repository)
+        tree-walk (org.eclipse.jgit.treewalk.TreeWalk. repository)
         ]
 
-    (.addTree treeWalk tree)
-    (.setRecursive treeWalk true)
+    (.addTree tree-walk tree)
+    (.setRecursive tree-walk true)
 
-    (take-while identity (repeatedly #(and (.next treeWalk) (.getPathString treeWalk))))))
+    (take-while identity
+                (repeatedly #(and (.next tree-walk)
+                                  (assoc (treewalk->file repository tree-walk)
+                                         :rev-str (->> commit .getId ObjectId/toString)))))))
 
-'(-<>>
+'((-<>>
  gcc-repo
  first-monthly-commits
  first
- (list-repository-contents gcc-repo)
- (take 10)
+ (repo-files gcc-repo)
+ (take 2)
  pprint
  time-mins
- )
+ ))
