bd420abffa949dc5ba6f23b28802f8e3012923e8
Dan Gopstein <dan@gopstein.com>
2017-10-18 08:52:29 -0400

    giant atom spikes

--- src/atom_finder/results_sandbox.clj
+++ src/atom_finder/results_sandbox.clj
@@ -10,50 +10,6 @@
    )
   )
 
-(->> "Gcc-atom-comment-context_2017-05-17_1.edn"
-     read-data
-     flatten-res
-     (map #(- (:ast-size-after %1) (:ast-size-before %1)))
-     (map prn))
-;(def mem-data-old mem-data)
-;(def commen-aggs-old comment-aggs)
-(comment
-(time (do
-(->> ;"gcc-atom-comment-context_2017-05-17_1.edn"
-     "gcc-atom-comment-context_2017-05-17_0.edn"
-     read-data
-     (def mem-data) time)
-(->> mem-data
-     flatten-res
-     ;(filter :ast-size-before) ; remove commits that didn't parse
-     (def flat-data) time)
-(->> flat-data
-     (map #(dissoc % :atom :rev-str :bug-ids :file))
-     (def comment-counts)
-     time
-     )
-(->> comment-counts
-     ;pprint)
-     (apply merge-with +)
-     (def comment-aggs)
-     time
-     )
-
-(pprint (sort comment-aggs))
-
-(pprint (map-values #(format "%05f" (float %))
-  {:inner-atom-comment-rate     (safe-div (:inner-comments-added-near-atoms comment-aggs)
-                                          (:inner-atom-count comment-aggs))
-   :inner-non-atom-comment-rate (safe-div (:inner-comments-added-away-atoms comment-aggs)
-                                          (:inner-ast-size comment-aggs))
-   :outer-atom-comment-rate (safe-div (:outer-comments-added-near-atoms comment-aggs)
-                                      (:outer-atom-count comment-aggs))
-   :outer-non-atom-comment-rate (safe-div (:outer-comments-added-away-atoms comment-aggs)
-                                          (:outer-ast-size comment-aggs))
-   }))
-
-))
-
 (defn file-level-key? [key] (not (re-find #"atom" (str key))))
 
 (def keep-numeric-vals (partial dissoc-by (comp not number? last)))
@@ -75,84 +31,40 @@
          ))
   )
 
+;; 2 mins?
+'((->> ;"code-age_gcc_2017-10-14_02.edn" (pap (constantly (now))) read-data
+     "src/data/code-age_gcc_2017-10-14_02.edn" read-lines
+;     (take 100)
+     (def code-age)
+     time-mins))
 
-(->> flat-data
-     ;(filter #(= :omitted-curly-braces (:atom %)))
-     (group-by (comp :atom))
-     (map-values stats-by-file-type)
-     (def file-type-stats)
-     time)
+'((->> code-age (filter (comp #{"fbda33410c6e44f9506739b3fe222bcd76c8db86" "ef9f1878ccac299b9190dc0d37a6f20951ac397f"} :rev-str)) (def big-change) time-mins))
 
-(do
-(println (clojure.string/join "," (concat ["atom" ".ext"] (->> file-type-stats first val first val keys))))
-(for [[atom exts] file-type-stats]
-  (for [[ext data] exts]
-    (println (clojure.string/join "," (concat [atom ext] (vals data)))))))
+'((->> big-change (group-by :path) (sort-by (fn [[k [v1 v2]]] (- (Math/abs (- (-> v1 :atoms :implicit-predicate (or 0)) (-> v2 :atoms :implicit-predicate (or 0))))))) (take 20) (map prn)))
 
+'((->> "/gcc/testsuite/gcc.dg/c99-intconst-1.c" (str gcc-path) parse-file (def c99-intconst-1)))
+'((->> c99-intconst-1 ((-> atom-lookup :implicit-predicate :finder)) (take 20) (def intconst-atoms)))
+'((->> intconst-atoms first))
+'((->> intconst-atoms last print-tree))
 
-(->> "~/opt/src/gcc/gcc/rtl.h"
-     parse-file
-     ((:finder (atom-lookup :omitted-curly-braces)))
-     (map write-ast)
-     (map (comp println println println))
-     )
+;; 30 secs | 14 mins all of gcc, lazily
+'((->>
+   code-age
+   (group-by (juxt :date :rev-str))
+   (map-values #(->> % (map :atoms) (apply merge-with +)))
+   (def atoms-by-month)
+   time-mins
+   ))
 
-(->> ;"version.h"
-     ;"ChangeLog"
- "mimetypes.default"
-     parse-file
-     print-tree)
-     ;flatten-tree
-     ;count)
+'((->> atoms-by-month keys count))
+'((->> atoms-by-month pprint))
 
-)
-
-(->> flat-data
-     (map (juxt :rev-str :file :ast-size-before :ast-size-after))
-     (filter (partial every? identity))
-     (map (fn [[rev file a b]] [rev file a b (- b a)]))
-     (apply max-key last)
-     pprint)
-
-(every? identity [1 2 nil])
-
-(apply max-key last [[1 2] [3 0]])
-
-(->> "/Users/dgopstein/opt/src/gcc/gcc/common/config/pdp11/pdp11-common.c"
-     "/Users/dgopstein/nyu/confusion/atom-finder/src/test/resources/implicit-predicate.c"
-     parse-file
-     ((default-finder implicit-predicate-atom?)))
-
-(print-atoms-in-dir gcc-path (select-values atom-lookup [:type-conversion]))
-
-(->> "/home/dgopstein/opt/src/gcc/libstdc++-v3/include/debug/safe_iterator.h"
-     (atoms-in-file atoms))
-
-(bigint 0xffff800000000000)
-
-(java.math.BigInteger. "0xffff800000000000" 16)
-
-;(->> "/home/dgopstein/opt/src/gcc/libgfortran/io/unix.c"
-;     parse-file
-(->> "static const struct stream_vtable x = {.read = (void *) raw_read};"
-     parse-frag
-     ;;print-tree)
-     ;(get-in-tree [0 1 1 0 0 1])
-     ;type-conversion-atom?
-     ((default-finder type-conversion-atom?))
-     )
-
-(->> "int x(char *y) { y; }"
-     parse-source
-     (get-in-tree [0 2 0 0])
-     .getExpressionType
-     )
-
-(->> ;"char *V1 = 33333333, V2, *V3"
-     "(int *) 2.3"
-     parse-frag
-     (def node))
-(->> node .getTypeId)
-(->> node .getDeclSpecifier)
-(->> node .getDeclarators (map (memfn getPointerOperators)) (filter empty?))
-(->> node type-conversion-atom?)
+'((->>
+   atoms-by-month
+   (map-values-kv (fn [[date rev-str] v] (merge v {:date date :rev-str rev-str})))
+   vals
+   (filter :date)
+   (sort-by :date)
+   (maps-to-csv "gcc-atoms-by-month.csv")
+   time-mins
+   ))
