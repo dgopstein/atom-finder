07b0e839209b9cbd5d09b49658f8171d2e5a677c
Dan Gopstein <dan@gopstein.com>
2017-10-25 22:06:14 -0400

    project atom distribution

--- src/analysis/atom_counts.R
+++ src/analysis/atom_counts.R
@@ -32,5 +32,10 @@ p4 <- chart.project('clang')
 p.labels <- ggplot(data=df, aes(x=atom, y=0)) + coord_fixed(ratio = 0) +
   theme(axis.title.y=element_text(colour="white", size=16), axis.text.y=element_blank(), axis.ticks.y=element_blank()) +
   theme(axis.ticks.x=element_blank(), axis.text.x = element_text(size=7, angle = 45, hjust = 1)) +
-  theme(plot.margin = unit(c(0,.2,2,.2), "cm"))
+  theme(plot.margin = mrgn)
 grid.arrange(p1, p2, p3, p4, p.labels, ncol = 1)
+
+
+
+t(data.frame(cbind(c(1,2),c(3,4))))
+ggplot(data)
