8f536d5d2a6c13f386254ab8d2433b09f35060ff
Dan Gopstein <dan@gopstein.com>
2018-03-16 19:29:05 -0400

    reduce colors in time charts

--- src/analysis/code_age.R
+++ src/analysis/code_age.R
@@ -65,9 +65,9 @@ mean.atoms.by.project.age <-
   ggplot(project.age.mean.atoms, aes(x=date, y=rate)) +
   theme_classic() +
   #geom_smooth(method="rlm", colour="black", size=0.5, se=FALSE, fullrange=TRUE) +
-  stat_smooth(colour="black", size=0.5, se=FALSE, fullrange=TRUE, 
+  stat_smooth(colour=colors2dark[1], size=0.5, se=FALSE, fullrange=TRUE,
     method=function(f,data=data,weights=weight) MASS::rlm(f,data,weights=weight,method="MM")) +
-  geom_point(size=3, aes(colour=domain)) +
+  geom_point(size=3, color=colors2dark[2]) +
   geom_text(aes(label=paste(" ", project)), size = 3, angle=-18, hjust=0, vjust=0.4) +
   #geom_text_repel(aes(label=project), size = 4, angle=-20, force=0.1, direction="x") +
   scale_x_date(limits = as.Date(c("1985-01-01", "2017-01-01"))) +
@@ -138,12 +138,12 @@ project.age.linear.no.intercept
 project.age.linear <- ggplot(code.age.all.atoms[project!='emacs']) +
   theme_classic() +
   stat_smooth(aes(date, rate), method="lm", colour="gray", size=2, se=FALSE, fullrange=TRUE) +
-  stat_smooth(aes(date, rate, group=project, color=project), method="lm", size=0.5, se=FALSE) +
-  annotate("text", x=as.Date('1991-02-01'), y=0.017, angle=-4.2, label="     All Projects", hjust=0.0, size=4.0) +
-  annotate("point", x=as.Date('1991-02-01'), y=0.01605, colour='gray', size=4.0) +
-  geom_point(aes(date, linear.rate, color=project), data=first.points[project!='emacs'], size=2) +
+  stat_smooth(aes(date, rate, group=project), color=colors2dark[1], method="lm", size=0.5, se=FALSE) +
+  annotate("text", x=as.Date('1991-02-01'), y=0.017, angle=-4.2, label="     All Projects", hjust=0.0, size=4) +
+  annotate("point", x=as.Date('1991-02-01'), y=0.01605, colour='#888888', size=4.0) +
+  geom_point(aes(date, linear.rate), color=colors2dark[2], data=first.points[project!='emacs'], size=2) +
   geom_text(aes(date, linear.rate, label=paste0("  ", project), angle=(1.3*(10^7)*angle)),
-            data=first.points[project!='emacs'], hjust=0, vjust=-0.4) +
+            data=first.points[project!='emacs'], hjust=0, vjust=-0.4, size=3.0) +
   labs(x = "Date", y = "Linearized Atom Rate") +
   guides(colour=FALSE)
 
@@ -158,4 +158,6 @@ ggplot(code.age.all.atoms) +
   stat_smooth(aes(date, rate, group=project, color=project), size=0.5, se=FALSE) +
   stat_smooth(aes(date, rate), method="lm", colour="red", size=2, se=FALSE, fullrange=TRUE)
 
-  
+Hmisc::binconf(20, 20)
+Hmisc::binconf(19, 20)
+Hmisc::binconf(12, 20)
