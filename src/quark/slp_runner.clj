f5b99ab161fccb788cbc3a7bb821103f6397e0c2
Dan Gopstein <dan@gopstein.com>
2018-03-31 12:48:47 -0400

    Fix static setter calls

--- src/quark/slp_runner.clj
+++ src/quark/slp_runner.clj
@@ -1,12 +1,12 @@
 ;; Use SLP-core to learn an N-gram model
 ;; Direct port of SLP-Core/src/main/java/slp/core/example/JavaRunner.java
 
-(ns quark.tree-tokenizer
-  (:require [atom-finder.util :refer :all]
-            [atom-finder.constants :refer :all]
-            [schema.core :as s]
+(ns quark.slp-runner
+  (:require [schema.core :as s]
             [clojure.pprint :refer [pprint]]
-            [clojure.string :as str])
+            [clojure.string :as str]
+            [atom-finder.util :refer :all]
+            )
   (:import
             [slp.core.counting.giga GigaCounter]
             [slp.core.lexing LexerRunner code.JavaLexer]
@@ -14,39 +14,45 @@
              ngram.JMModel mix.InverseMixModel mix.NestedModel]
             ))
 
+(defn pair->vec [p] [(.left p) (.right p)])
+
 (time-mins
  (do
                                         ;
 ;(def train (java.io.File. (expand-home "~/atom-finder/src/java")))
-(def train (java.io.File. (expand-home "~/opt/src/SLP-Core")))
-(def test train)
+(def train-set (java.io.File. (expand-home "~/atom-finder/src/java")))
+(def test-set train-set)
 
-(. LexerRunner
-  (setLexer (JavaLexer.))
-  (setPerLine false)
-  (addSentenceMarkers true)
-  (useExtension "java"))
+(. LexerRunner (setLexer (JavaLexer.)))
+(. LexerRunner (setPerLine false))
+(. LexerRunner (addSentenceMarkers true))
+(. LexerRunner (useExtension "java"))
 
 (def model (JMModel. (GigaCounter.)))
 
-(. ModelRunner
-   (perLine false)
-   (selfTesting (train.equals test))
-   (setNGramOrder 6)
-   (learn model train))
+(. ModelRunner (perLine false))
+(. ModelRunner (selfTesting (.equals train-set test-set)))
+(. ModelRunner (setNGramOrder 6))
+(. ModelRunner (learn model train-set))
 
-(def model (NestedModel. test model))
+(def model (NestedModel. test-set model))
 (pprn model)
 (def model (InverseMixModel. model (CacheModel.)))
 (.setDynamic model true)
 
-(def modeledFiles (ModelRunner/model model test))
-(pprn modeledFiles)
-(def statistics (ModelRunner/getStats modeledFiles))
+(def modeledFiles (ModelRunner/model model test-set))
+
+(def mf (->> modeledFiles .iterator iterator-seq (map pair->vec) (into {})))
+;(def mf2 mf)
+
+(def statistics (ModelRunner/getStats mf))
 (pprn statistics)
 
 (printf "Modeled %d tokens, average entropy:\t%.4f\n" (.getCount statistics) (.getAverage statistics))
 )
 )
 
-(slp.core.example.JavaRunner/main (into-array String [(expand-home "~/opt/src/SLP-Core")]))
+;(slp.core.example.JavaRunner/main (into-array String [(expand-home
+;                                                       ;"~/opt/src/SLP-Core"
+;                                                       "~/atom-finder/src/java"
+;                                                       )]))
