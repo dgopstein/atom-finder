f382b29c6e9861c0ebae37bf439942dd86a7f30d
Dan Gopstein <dan@gopstein.com>
2018-04-11 09:11:09 -0400

    atoms vs n-gram probability

--- src/analysis/entropy.R
+++ src/analysis/entropy.R
@@ -1,40 +1,64 @@
 library(data.table)
 library(ggplot2)
 library(viridis)
+library(ggridges)
 
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
-stream.file <- function(filename, n.lines, init, fun) {
-  f = file(filename, "r")
-  
-  accumulator <- init
-  stop = FALSE
-  iter = 0
-  while(!stop) {
-    print(paste("processed lines: ", n.lines * iter));
-    iter <- iter + 1;
-    
-    next.lines = readLines(f, n = n.lines)
+line.entropies <- data.table(read.csv("../../tmp/atom-finder-token-probabilities_gecko-dev.csv", sep="|"))
 
-    accumulator <- fun(accumulator, next.lines)
-
-    if(length(next.lines) < n.lines) {
-      stop = TRUE
-      close(f)
-    }
-  }
-  
-  accumulator
-}
-
-# cat atom-finder-token-probabilities_gecko-dev.csv | cut -d\| -f3 | grep -o '[^][]\+' > atom-finder-token-probabilities_gecko-dev_only-probs.txt
-line.entropies <-
-  stream.file("../../tmp/atom-finder-token-probabilities_gecko-dev_only-probs.txt", n.lines = 100000, init = c(),
-            function(line.entropies, next.lines) {
-              line.entropy <- unlist(lapply(strsplit(next.lines, ", "), function(x) mean(as.numeric(x))))
-              c(line.entropies, line.entropy)
-            })
+line.entropies[, probability := sapply(strsplit(gsub("]|\\[","",probability), ", "), function(x) mean(as.numeric(x)))]
 
 plot(density(line.entropies, na.rm=TRUE), xlim=c(0, 12))
 
+line.nodes <- data.table(read.csv("../../tmp/gecko-node-lines.csv"))
 
+line.node.entropy <- merge(line.nodes, line.entropies, by=c("file", "line"), all=TRUE)
+line.node.entropy <- line.node.entropy[file!="anonymously-parsed-code.c"]
+line.node.entropy[, any.atom := atom!="non-atoms"]
+
+
+ast.probability <- line.node.entropy[, .(prob = mean(probability, na.rm=TRUE)), by="ast"]
+
+ggplot(ast.probability) +
+  geom_bar(aes(reorder(ast, prob), prob), stat="identity") +
+  coord_flip()
+
+atom.probability <- line.node.entropy[, .(prob = mean(probability, na.rm=TRUE)), by="atom"]
+
+ggplot(atom.probability) +
+  geom_bar(aes(reorder(atom, prob), prob), stat="identity") +
+  coord_flip()
+
+file.node.entropy <- line.node.entropy[, .(atom=atom[1], probability = min(probability)), by=c("file","line")][, .(atom = mean(atom!="non-atoms", na.rm=TRUE), probability = mean(probability, na.rm=TRUE)), by=file]
+
+ggplot(file.node.entropy, aes(atom, probability)) +
+  geom_density_2d() +
+  stat_density_2d(geom = "raster", aes(fill = ..density..), n = 500, contour = FALSE) +
+  scale_fill_viridis(guide=FALSE) +
+  xlim(0,0.1)
+
+ggplot(line.node.entropy[!is.na(atom) & !is.na(probability)], aes(x = probability, y = atom, group = atom, fill = ..x..)) + 
+  geom_density_ridges_gradient(scale = 1.5, rel_min_height = 0.01) +
+  scale_fill_viridis(name = "Probability", option = "C", guide=FALSE) +
+  scale_x_continuous(limits = c(-1, 12)) +
+  labs(title = 'Predictability of Files') +
+  theme_ridges(grid = FALSE, center_axis_labels = TRUE)
+
+ggplot(line.node.entropy[!is.na(any.atom) & !is.na(probability)], aes(x=probability, fill=any.atom)) +
+  geom_density(alpha=0.25, bw=.5) +
+  scale_x_continuous(limits=c(-1,12))
+
+
+
+atom.rates.probability <- all.atom.rates
+setDT(atom.rates.probability, keep.rownames = TRUE)[]
+colnames(atom.rates.probability) <- c("atom", "display.name", "rate")
+atom.rates.probability[, atom := gsub("\\.", "-", atom)]
+atom.rates.probability <- merge(atom.rates.probability, atom.probability, by="atom")
+atom.rates.probability <- merge(atom.rates.probability, atom.effect.sizes[, .(atom = gsub("\\.", "-", atom), effect.size = effect.size)], by = "atom")
+
+ggplot(atom.rates.probability, aes(log(rate), prob)) +  geom_point()
+ggplot(atom.rates.probability, aes(effect.size, prob)) +  geom_point()
+
+  
